757381b106e0bbdbc9953f2eec464bbb4a628b51
Dihep <dihep.demayo@gmail.com>
2024-04-22 22:43:20 +0200

    Completed excercise.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-let m = document.getElementById("container");
+const m = document.getElementById("container");
 console.log(m);
 for (i=1; i<=100; i++) {
-    m.append("appended");
-    console.log(m);
+    const newArt = document.createElement("article");
+    m.append(newArt);
+    let content = i;
+    if ( (i%15)==0 ) {
+        content = "fizzbuzz";
+        console.log("15th");
+    }
+    else if (i%3==0) {
+        content = "fizz";
+        console.log("3th");
+    }
+    else if (i%5==0) {
+        content = "buzz";
+        console.log("5th");
+    }
+    newArt.append(content);
 };
